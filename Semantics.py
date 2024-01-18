c1cf1ad30fa2a44b12c9cb5ee3a57016f1bbab07
Ahmed11uu <113991347+Ahmed11uu@users.noreply.github.com>
2024-01-18 22:13:32 +0000

    semantics modif

--- Semantics.py
+++ Semantics.py
@@ -1,19 +1,54 @@
+# Semantics.py
+
 from abc import ABC, abstractmethod
 
-
 class SemanticRelation(ABC):
+    """
+    Abstract base class for semantic relations.
+
+    Defines abstract methods that must be implemented by subclasses.
+    """
+
     @abstractmethod
     def __init__(self):
+        """
+        Abstract constructor method.
+        """
         pass
 
     @abstractmethod
     def initial(self):
+        """
+        Abstract method to get the initial state.
+
+        Returns:
+        - The initial state.
+        """
         pass
 
     @abstractmethod
-    def actions(self,src):
+    def actions(self, src):
+        """
+        Abstract method to get possible actions for a given state.
+
+        Parameters:
+        - src: The current state.
+
+        Returns:
+        - A list containing possible actions.
+        """
         pass
 
     @abstractmethod
-    def execute(self,actions,src):
+    def execute(self, actions, src):
+        """
+        Abstract method to execute an action on the current state.
+
+        Parameters:
+        - actions: The action to be executed.
+        - src: The current state.
+
+        Returns:
+        - The result of executing the action on the current state.
+        """
         pass
